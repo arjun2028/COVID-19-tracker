dbdd6048929471441f63686c29bbb6d42de0f24e
arjun2028 <arjun9650829@gmail.com>
2021-08-31 23:27:11 +0530

    Complete App

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDqREI1pab4YLT2Cz8grXJVevMs9ekhYkQ",
+  authDomain: "covid-tracker-8d49f.firebaseapp.com",
+  projectId: "covid-tracker-8d49f",
+  storageBucket: "covid-tracker-8d49f.appspot.com",
+  messagingSenderId: "918360457775",
+  appId: "1:918360457775:web:fb794827034b5539c5bcda",
+  measurementId: "G-82XP5B7D1F",
+};
